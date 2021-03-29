c9dcaca89997cc6f6f9989f875ba6aae40b2b9f7
LeeChangjoo <karekano02@hanmail.net>
2021-03-29 18:02:23 +0900

    ex

---
+++ MultiArrEx3.java
@@ -0,0 +1,21 @@
+class MultiArrEx3{
+    public static void mina(String[] arg){
+        int [][] m1 = {
+            {1,2,3,},
+            {4,5,6,}
+        };
+
+        int [][] m2 = {
+            {1,2},
+            {3,4},
+            {5,6}
+        };
+
+        final int ROW = m1.length;
+        final int COL = m2[0].length;
+
+        int[][] m3 = new int[ROW][COL];
+
+
+    }
+}
