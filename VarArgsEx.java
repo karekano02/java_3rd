fe717994ea16e700e214e8e93332ffbdbfc705f8
LeeChangjoo <karekano02@hanmail.net>
2021-03-31 10:50:53 +0900

    가변인자와 오버로드

---
+++ VarArgsEx.java
@@ -0,0 +1,18 @@
+//가변인자와 오버로드
+public class VarArgsEx {
+   public static void main(String[] args) {
+       String[] strArr = {"100", "200", "300"};
+
+       System.out.println(concatenate("~~~", "100", "200", "300"));
+   } 
+
+   static String concatenate(String delim, String... args){
+       String result = "";
+
+       for(String str : args){
+           result += str + delim;
+       }
+
+       return result;
+   }
+}
