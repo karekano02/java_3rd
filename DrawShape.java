aa4a093cb4ef72b4ba823d727653edada735b512
LeeChangjoo <karekano02@hanmail.net>
2021-04-01 10:52:19 +0900

    조상 클래스 생성자

--- DrawShape.java
+++ DrawShape.java
@@ -45,7 +45,7 @@ class Triangle extends Shape{
     Triangle(Point[] p){
         this.p = p;
     }
-
+//오버라이딩
     void draw(){
         System.out.printf("[p1=%s, p2=%s, p3=%s, color=%s]%n", p[0].getXY(), p[1].getXY(), p[2].getXY(), color);
     }
