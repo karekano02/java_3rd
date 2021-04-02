3ab4d286e6c6b7993e5a3a5e9890da575a9df761
LeeChangjoo <karekano02@hanmail.net>
2021-04-02 10:58:50 +0900

    다형성 (매개변수)  + 여러종류 객체를 배열에 + Vector class

--- PolyArgumentTest.java
+++ PolyArgumentTest.java
@@ -1,4 +1,6 @@
-//다형성 (매개변수)  + 여러종류 객체를 배열에 
+import java.util.*;
+
+//다형성 (매개변수)  + 여러종류 객체를 배열에 + Vector class 
 class Product{
     int price;
     int bounsPoint;
@@ -36,8 +38,10 @@ class Audio extends Product{
 class Buyer{
     int money = 1000;
     int bounsPoint = 0;
-    Product[] pArr = new Product[10];
-    int count = 0;
+    // Product[] pArr = new Product[10];
+    // int count = 0;
+
+    Vector pArr = new Vector(); // Vector 동적으로 크기가 관리되는 객체배열일 뿐
 
     void buy(Product p){
         if(money < p.price ){
@@ -47,7 +51,8 @@ class Buyer{
 
         money -= p.price;
         bounsPoint += p.bounsPoint;
-        pArr[count++] = p;
+        // pArr[count++] = p;
+        pArr.add(p);
         System.out.println(p + "을/를 구입하셧습니다.");
     }
 
@@ -55,23 +60,54 @@ class Buyer{
         int sum = 0;    // 지역변수는 반드시 초기화
         String itemList = "";
 
-        for(Product item : pArr){
-            if(item == null) break;
-            sum += item.price;
-            itemList += item.toString() + ", ";
+        // for(Product item : pArr){
+        //     if(item == null) break;
+        //     sum += item.price;
+        //     itemList += item.toString() + ", ";
+        // }
+
+        for(int i = 0 ; i < pArr.size() ; i++){
+            Product p = (Product)pArr.get(i);
+            sum += p.price;
+            if(i == pArr.size() - 1)
+                itemList += p.toString();
+            else
+                itemList += p.toString() + ", ";
         }
 
         System.out.println("구입하신 물품의 총금액은 " + sum + "만원입니다. ");
         System.out.println("구입하신 제품은 " + itemList + "입니다.");
+    }
+
+    void respond(Product p){
+        if(pArr.remove(p)){
+            money += p.price;
+            bounsPoint -= p.bounsPoint;
+            System.out.println(p + "을/를 반품하셨습니다.");
+        }else{
+
+        }
     }
 }
 
 public class PolyArgumentTest {
     public static void main(String[] args) {
         Buyer b = new Buyer();
-        b.buy(new Tv()); // 자동형 변환
-        b.buy(new Computer());
-        b.buy(new Audio());
+
+        Product tv = new Tv(); // 
+        Product computer = new Computer();
+        Product audio = new Audio();
+
+        b.buy(tv); // 자동형 변환
+        b.buy(computer);
+        b.buy(audio);
+
+        b.Sum();
+
+        System.out.println("현재 남은 돈은 " + b.money + "만원입니다.");
+        System.out.println("현재 남은 포인트는 " + b.bounsPoint + "점입니다.");
+
+        b.respond(tv);
 
         b.Sum();
 
