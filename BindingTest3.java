42021e305f47e91d5323af025cd6f6ecabce2c82
LeeChangjoo <karekano02@hanmail.net>
2021-04-02 10:31:16 +0900

    다형성 + 여러종류 객체 배열

--- BindingTest3.java
+++ BindingTest3.java
@@ -25,7 +25,7 @@ class Child extends Parent{
     int x = 200;
 
     void method(){
-        System.out.println("x = " + x);
+        System.out.println("x = " + x); //this.x
         System.out.println("super.x = " +super.x);
         System.out.println("this.x = " + this.x);
     }
--- PolyArgumentTest.java
+++ PolyArgumentTest.java
@@ -1,4 +1,4 @@
-//다형성 (매개변수)
+//다형성 (매개변수)  + 여러종류 객체를 배열에 
 class Product{
     int price;
     int bounsPoint;
@@ -25,9 +25,19 @@ class Computer extends Product{
     public String toString(){return "Computer";}
 }
 
+class Audio extends Product{
+    Audio(){
+        super(50);
+    }
+
+    public String toString(){return "Audio";}
+}
+
 class Buyer{
     int money = 1000;
     int bounsPoint = 0;
+    Product[] pArr = new Product[10];
+    int count = 0;
 
     void buy(Product p){
         if(money < p.price ){
@@ -37,7 +47,22 @@ class Buyer{
 
         money -= p.price;
         bounsPoint += p.bounsPoint;
+        pArr[count++] = p;
         System.out.println(p + "을/를 구입하셧습니다.");
+    }
+
+    void Sum(){
+        int sum = 0;    // 지역변수는 반드시 초기화
+        String itemList = "";
+
+        for(Product item : pArr){
+            if(item == null) break;
+            sum += item.price;
+            itemList += item.toString() + ", ";
+        }
+
+        System.out.println("구입하신 물품의 총금액은 " + sum + "만원입니다. ");
+        System.out.println("구입하신 제품은 " + itemList + "입니다.");
     }
 }
 
@@ -46,6 +71,9 @@ public class PolyArgumentTest {
         Buyer b = new Buyer();
         b.buy(new Tv()); // 자동형 변환
         b.buy(new Computer());
+        b.buy(new Audio());
+
+        b.Sum();
 
         System.out.println("현재 남은 돈은 " + b.money + "만원입니다.");
         System.out.println("현재 남은 포인트는 " + b.bounsPoint + "점입니다.");
