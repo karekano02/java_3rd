a484edf2812077ed4b68e3f83a6f5b5b99e1cad6
LeeChangjoo <karekano02@hanmail.net>
2021-04-06 17:08:50 +0900

    Exception 예외처리의 필요에 따라 선택 변경 + 연결된 예외

--- NewExceptionTest.java
+++ NewExceptionTest.java
@@ -1,29 +1,52 @@
-// 새롭게 발전하는 Exception 예외처리의 필요에 따라 선택 변경
+import javax.management.RuntimeErrorException;
+
+// 새롭게 발전하는 Exception 예외처리의 필요에 따라 선택 변경 + 연결된 예외
 public class NewExceptionTest {
 
     public static void main(String[] args) {
         try{
-            startInstall();
-            copyFile();
-        }catch(SpaceException e){
-            System.out.println("에러 메시지 : " + e.getMessage());
+            install();
+        }catch(InstallException e){
             e.printStackTrace();
-            System.out.println("공간을 확보한 후에 다시 설치하지기 바랍니다.");   
-        }catch(MemoryException e){
-            System.out.println("에러 메시지 : " + e.getMessage());
+        }catch(Exception e){
             e.printStackTrace();
-            System.gc();
-            System.out.println("다시 설치를 시도하세요.");
         }finally{
             deleteTempFiles();
         }
     } 
 
+    static void install() throws InstallException{
+        try{
+            startInstall();
+            copyFile();  
+        }catch(SpaceException e){
+            // System.out.println("에러 메시지 : " + e.getMessage());
+            // e.printStackTrace();
+            // System.out.println("공간을 확보한 후에 다시 설치하지기 바랍니다.");             
+            InstallException ie = new InstallException("설치 중 예외 발생");
+            ie.initCause(e);
+            throw ie;
+        }catch(MemoryException e){
+        // }catch(RuntimeErrorException e){
+            // System.out.println("에러 메시지 : " + e.getMessage());
+            // e.printStackTrace();
+            // System.gc();
+            // System.out.println("다시 설치를 시도하세요.");
+            InstallException ie = new InstallException("설치 중 예외 발생");
+            ie.initCause(e);
+            throw ie;
+        }finally{
+            deleteTempFiles();
+        }
+    }
+
     static void startInstall() throws SpaceException, MemoryException{
+    // static void startInstall() throws SpaceException{
         if(!enoughspace())
             throw new SpaceException("설치할 공간이 부족합니다.");
         if(!enoughMemory())
             throw new MemoryException("메모리가 부족합니다.");
+            // throw new RuntimeException(new MemoryException("메모리가 부족합니다."));
     }
     static void copyFile(){}
     static void deleteTempFiles(){}
@@ -32,6 +55,12 @@ public class NewExceptionTest {
     }
     static boolean enoughMemory(){
         return false;
+    }
+}
+
+class InstallException extends Exception{
+    InstallException(String msg){
+        super(msg);
     }
 }
 
