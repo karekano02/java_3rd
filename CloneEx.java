db688eae284e2e34f4708a18e78c222a2a891e0d
LeeChangjoo <karekano02@hanmail.net>
2021-04-07 11:16:51 +0900

    공변 반환 타입

--- CloneEx.java
+++ CloneEx.java
@@ -1,4 +1,4 @@
-// clone 예제 
+// clone 예제 + 공변 반환 타입
 
 class Point implements Cloneable{
     int x, y;
@@ -12,21 +12,23 @@ class Point implements Cloneable{
         return "x = " + x + ", y = " + y; 
     }
 
-    public Object clone(){
+    // public Object clone(){
+    public Point clone(){   // 공변 반환타입 자손 객체로 반환 가능 
         Object obj = null;
 
         try {
             obj = super.clone();
         }catch(CloneNotSupportedException e){}
 
-        return obj;
+        return (Point)obj;
     }
 }
 public class CloneEx {
     public static void main(String[] args) {
         Point original = new Point(3, 5);
 
-        Point temp = (Point)original.clone();
+        // Point temp = (Point)original.clone();
+        Point temp = original.clone();
 
         System.out.println(original);
         System.out.println(temp);
