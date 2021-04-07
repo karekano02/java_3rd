5b9d02b36d8f1cc60c8d3f6b19a7f44799f66c74
LeeChangjoo <karekano02@hanmail.net>
2021-04-07 18:01:57 +0900

    stringbuffer

--- StringEx3.java
+++ StringEx3.java
@@ -1,4 +1,4 @@
-//String 기능 substring
+//String 기능 substring stringbuffer
 public class StringEx3 {
     public static void main(String[] args) {
         String fullName = "hello.java";
@@ -11,6 +11,17 @@ public class StringEx3 {
 
         System.out.println(fileName);
         System.out.println(ext);
+
+        StringBuffer sb = new StringBuffer("abc");
+        StringBuffer sb2 = new StringBuffer("abc");
+
+        System.out.println("sb = sb2 ::" + (sb==sb2));
+        System.out.println("sb.equals(sb2) ::" + sb.equals(sb2));
+
+        String s = sb.toString();
+        String s1 = sb2.toString();
+
+        System.out.println(s.equals(s1));
 
     }
 }
