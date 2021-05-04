cc8c376713ed31602da7e468af412d0930a9530a
LeeChangjoo <karekano02@hanmail.net>
2021-05-04 14:05:03 +0900

    Enum 열거형 예제들

--- EnumEx1.java
+++ EnumEx1.java
@@ -1,6 +1,42 @@
-// Enum 열거형 예제
+// Enum 열거형 예제들 
 enum Direction {EAST, SOUTH,WEST,NORTH}
 
+enum Direction_1{
+    EAST(1,">"), SOUTH(2, "V"), WEST(3,"<"), NORTH(4,"^");
+
+    private static final Direction_1[] DIR_ARR = Direction_1.values();
+    private final int value;
+    private final String symbol;
+
+    Direction_1(int value, String symbol){
+        this.value = value;
+        this.symbol = symbol;
+    }
+
+    public int getValue(){return value;}
+    public String getSymbol(){return symbol;}
+
+    public static Direction_1 of(int dir){
+        if(dir< 1|| dir > 4){
+            throw new IllegalArgumentException("Invalid value :" + dir);
+        }
+        return DIR_ARR[dir-1];
+    }
+
+    // 방향을 회전시키는 메서드. num의 값만큼 90도씩 시계방향으로 회전한다.
+    public Direction_1 rotate(int num){
+        num = num % 4;
+
+        if(num < 0) num +=4;
+
+        return DIR_ARR[(value-1+num)%4];
+
+    }
+
+    public String toString(){
+        return name()+getSymbol();
+    }
+}
 public class EnumEx1 {
     public static void main(String[] args) {
         Direction d1 = Direction.EAST;
@@ -34,5 +70,22 @@ public class EnumEx1 {
             System.out.println(d.name() + " = " + d.ordinal());
         }
 
+        System.out.println();
+
+        //Ex 2
+
+        for (Direction_1 d : Direction_1.values())
+            System.out.println(d.name() + " : " + d.getValue());
+
+        System.out.println();
+        Direction_1 d1_1 = Direction_1.EAST;
+        Direction_1 d2_1 = Direction_1.of(2);
+            
+        System.out.println(d1_1.name() + " : " + d1_1.getValue());
+        System.out.println(d2_1.name() + " : " + d2_1.getValue());
+        System.out.println(Direction_1.EAST.rotate(1));
+        System.out.println(Direction_1.EAST.rotate(2));
+        System.out.println(Direction_1.EAST.rotate(-1));
+        System.out.println(Direction_1.EAST.rotate(-2));
     }
 }
