56230bc9bcc4f13e7dc29b0d3c21ad50ddb5d44b
LeeChangjoo <karekano02@hanmail.net>
2021-05-06 17:41:33 +0900

    Thread 기본 예제 (우선순위 포함)

--- ThreadEx.java
+++ ThreadEx.java
@@ -1,4 +1,4 @@
-// Thread 기본 예제
+// Thread 기본 예제 (우선순위 포함)
 public class ThreadEx {
     public static void main(String[] args){
         Thread1_1 t1 = new Thread1_1();
@@ -6,12 +6,8 @@ public class ThreadEx {
         Runnable r = new Thread1_2();
         Thread t2 = new Thread(r);
 
+        t2.setPriority(7);   // main 은 default 5  높을 수록 우선순위 
         t1.start();
-        try {
-            Thread.sleep(10);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
         t2.start();
     }
 }
