b649db7d66b2b2ee1f49a23d99a6d20309ce72b3
kareakno02 <karekano02@hanmail.net>
2021-05-19 17:42:07 +0900

    LambdaEx5 함수형 인터페이스

--- LambdaEx5.java
+++ LambdaEx5.java
@@ -7,8 +7,8 @@ public class LambdaEx5 {
         Supplier<Integer> s = () -> (int)(Math.random()*100)+1;
         Consumer<Integer> c = i -> System.out.print(i + ", ");
         Predicate<Integer> p = i -> i%2==0;
-        Function<Integer, Integer> f = i -> i /10*10; // i의 일의 자리를 없앤다.
-        
+        // Function<Integer, Integer> f = i -> i /10*10; // i의 일의 자리를 없앤다.
+        UnaryOperator<Integer> f = i -> i /10*10;
 
         List<Integer> list = new ArrayList<>();
         makeRandomList(s, list);
@@ -20,7 +20,8 @@ public class LambdaEx5 {
         System.out.println("newList : " + newList);
     }
 
-    static <T> List<T> doSomething(Function<T, T> f, List<T> list){
+    static <T> List<T> doSomething(UnaryOperator<T> f, List<T> list){
+    // static <T> List<T> doSomething(Function<T, T> f, List<T> list){
         List<T> newList = new ArrayList<T>(list.size());
 
         for(T i : list){
